0dd6c953fd028566253350b2529f63b825916a5a
AntonioFarfan <70988355+AntonioFarfan@users.noreply.github.com>
2020-09-13 17:09:53 -0500

    Creacion de las clases e implementacion del button "Guardar Informacion" (#1)
    Co-authored-by: willian pandi <willian.pandi@unach.edu.ec>

---
+++ HerramientasAmbienteColaborativas/DatoAutomovil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerramientasAmbienteColaborativas
+{
+    public class DatoAutomovil
+    {
+        public string Conductor { set; get; }
+        public string Modelo { set; get; }
+        public string Marca { set; get; }
+        public string Placa { set; get; }
+        public int Kilometraje { set; get; }
+        public string FechaCompra { set; get; }
+
+        public DatoAutomovil()
+        {
+        }
+
+        public DatoAutomovil(string conductor, string modelo, string marca, string placa, int kilometraje, string fechaCompra)
+        {
+            Conductor = conductor;
+            Modelo = modelo;
+            Marca = marca;
+            Placa = placa;
+            Kilometraje = kilometraje;
+            FechaCompra = fechaCompra;
+        }
+    }
+}
